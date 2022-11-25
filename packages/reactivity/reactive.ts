c75cc0341300e8e24553c2c060241ea84e8eb3ea
HLN177 <harryleen177@gmail.com>
2022-11-25 20:31:30 +0000

    feat: extract track func and trigger func

--- packages/reactivity/reactive.ts
+++ packages/reactivity/reactive.ts
@@ -29,39 +29,46 @@ function effect(fn: Function) {
 function reactive(data: Object): any {
   return new Proxy(data, {
     get: function (target, key) {
-      // return directly if activeEffect no existed
-      if (!activeEffect) {
-        return Reflect.get(target, key);
-      }
-      // 1. get depsMap from bucket by "target", depsMap is a "Map"
-      if (!bucket.has(target)) {
-        bucket.set(target, new Map());
-      }
-      const depMap = bucket.get(target);
-      // 2. get deps from depsMap by "key", deps is a "Set"
-      // deps saves all the side effect function which related to the "key"
-      if (!depMap!.has(key)) {
-        depMap!.set(key, new Set());
-      }
-      const deps = depMap!.get(key);
-      // 3. save the current active effect function into bucket
-      deps!.add(activeEffect);
-
+      track(target, key);
       return Reflect.get(target, key);
     },
     set: function(target, key, newVal) {
       Reflect.set(target, key, newVal);
-      if (bucket.has(target)) {
-        // 1. get depsMap from bucket by "target"
-        const depMap = bucket.get(target);
-        // 2. get effects from depsMap by "key"
-        const effects: Set<any> | undefined = depMap!.get(key);
-        // 3. validation and run the related effect functions
-        effects && effects.forEach(fn => fn && fn());
-      }
+      trigger(target, key);
       return true;
     }
   });
+}
+
+function track(target: Object, key: any) {
+  // return directly if activeEffect no existed
+  if (!activeEffect) {
+    return Reflect.get(target, key);
+  }
+  // 1. get depsMap from bucket by "target", depsMap is a "Map"
+  if (!bucket.has(target)) {
+    bucket.set(target, new Map());
+  }
+  const depMap = bucket.get(target);
+  // 2. get deps from depsMap by "key", deps is a "Set"
+  // deps saves all the side effect function which related to the "key"
+  if (!depMap!.has(key)) {
+    depMap!.set(key, new Set());
+  }
+  const deps = depMap!.get(key);
+  // 3. save the current active effect function into bucket
+  deps!.add(activeEffect);
+}
+
+function trigger(target: Object, key: any) {
+  if (bucket.has(target)) {
+    // 1. get depsMap from bucket by "target"
+    const depMap = bucket.get(target);
+    // 2. get effects from depsMap by "key"
+    const effects: Set<any> | undefined = depMap!.get(key);
+    // 3. validation and run the related effect functions
+    effects && effects.forEach(fn => fn && fn());
+  }
 }
 
 export {
